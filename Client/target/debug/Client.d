95cbaf87f7a85a6a5a6b63ac6e9e5d9d5e16709b
ThibaudLAMBERT <thibaud.lambert.25@gmail.com>
2026-01-07 17:21:17 +0100

    add gitignore

---
+++ Client/target/debug/Client.d
@@ -0,0 +1,1 @@
+C:\Users\thiba\OneDrive\Bureau\Rust_Audio_Chat\Client\target\debug\Client.exe: C:\Users\thiba\OneDrive\Bureau\Rust_Audio_Chat\Client\src\main.rs
---
+++ Client/target/debug/deps/Client.d
@@ -0,0 +1,5 @@
+C:\Users\thiba\OneDrive\Bureau\Rust_Audio_Chat\Client\target\debug\deps\Client.d: src\main.rs
+
+C:\Users\thiba\OneDrive\Bureau\Rust_Audio_Chat\Client\target\debug\deps\Client.exe: src\main.rs
+
+src\main.rs:
